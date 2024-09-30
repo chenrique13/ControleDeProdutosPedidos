d55a3b4242bdaaa6d4959b515ef299bdc9faee8d
Carlos Pereira <c.henrique1309@gmail.com>
2024-09-29 23:59:54 -0300

    Create Departamento.java
    Criação da classe departamento com seus atributos e métodos básicos.

---
+++ src/main/java/br/com/kLab/controleDeProdutosPedidos/entidades/Departamento.java
@@ -0,0 +1,133 @@
+package br.com.kLab.controleDeProdutosPedidos.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * Classe representando um departamento do sistema de Controle de
+ * Produtos/Pedidos.
+ * 
+ * @author Carlos Pereira
+ */
+
+@Entity
+public class Departamento implements Serializable{
+
+	private static final long serialVersionUID = 102198882488252219L;
+
+	/**
+	 * Identificador unico do Departamento.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
+	
+	/**
+	 * Descricao do Departamento.
+	 */
+	private String descricao;
+
+	/**
+	 * Construtor padrão sem argumentos.
+	 */
+	public Departamento() {
+		
+	}
+	
+	/**
+	 * Construtor com todos os argumêntos.
+	 *
+	 * @param codigo
+	 * @param descricao
+	 */
+	public Departamento(Integer codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	/**
+	 * Obtem o codigo do Departamento.
+	 *
+	 * @return Integer
+	 */
+	public Integer getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * Define o codigo do Departamento.
+	 * 
+	 * @param codigo
+	 */
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+
+	/**
+	 * Obtem a descricao do Departamento.
+	 *
+	 * @return String
+	 */
+	public String getDescricao() {
+		return descricao;
+	}
+
+	/**
+	 * Define a descricao do Departamento.
+	 * 
+	 * @param descricao
+	 */
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	/**
+	 * Gera um hash de um Departamento a partir do seu codigo.
+	 * 
+	 * @return int
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo);
+	}
+
+	/**
+	 * Verifica se um Departamento e igual ao outro pelo codigo.
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departamento other = (Departamento) obj;
+		return Objects.equals(codigo, other.codigo);
+	}
+
+	/**
+	 * Retorna uma representação em texto do Departamento.
+	 * 
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Departamento [codigo=");
+		builder.append(codigo);
+		builder.append(", descricao=");
+		builder.append(descricao);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+}
