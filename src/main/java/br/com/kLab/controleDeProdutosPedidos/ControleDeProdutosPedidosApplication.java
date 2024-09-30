7db90af16787738d3f9391e911b0762dbdd20feb
Carlos Pereira <c.henrique1309@gmail.com>
2024-09-29 23:47:12 -0300

    Configurações iniciais
    Commit com as configurações iniciais do projeto de controle de produtos e pedidos.

---
+++ src/main/java/br/com/kLab/controleDeProdutosPedidos/ControleDeProdutosPedidosApplication.java
@@ -0,0 +1,13 @@
+package br.com.kLab.controleDeProdutosPedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeProdutosPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeProdutosPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kLab/controleDeProdutosPedidos/ControleDeProdutosPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kLab.controleDeProdutosPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeProdutosPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
